cef05f6503005d72e32c641a5dbfacb761b78a1f
Juliano Pestili <julianocp@ciandt.com>
2022-03-03 16:19:01 -0300

    Aula 3

--- AlunosAPI/Controllers/AlunoController.cs
+++ AlunosAPI/Controllers/AlunoController.cs
@@ -1,3 +1,4 @@
+using AlunosAPI.Models;
 using AlunosAPI.Services;
 using Microsoft.AspNetCore.Mvc;
 
@@ -5,12 +6,101 @@ namespace AlunosAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AlunoController: ControllerBase
+    public class AlunoController : ControllerBase
     {
         private IAlunoService _alunoService;
         public AlunoController(IAlunoService alunoService)
         {
             _alunoService = alunoService;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<IAsyncEnumerable<Aluno>>> GetAlunos()
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunos();
+                return Ok(alunos);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter alunos");
+            }
+        }
+
+        [HttpGet("AlunosPorNome")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<Aluno>> GetAlunoByName([FromQuery] string name)
+        {
+            try
+            {
+                var alunos = await _alunoService.GetAlunosByNome(name);
+                if (alunos == null || alunos.Count() == 0)
+                    return NotFound($"Não existe alunos com esse critério {alunos}");
+                else
+                    return Ok(alunos);
+            }
+            catch (System.Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Erro ao obter aluno");
+            }
+        }
+
+        [HttpGet("{id:int}", Name = "GetAluno")]
+        public async Task<ActionResult<Aluno>> GetAluno(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+                if (aluno == null)
+                    return NotFound($"Não existe alunos com esse id {id}");
+                else
+                    return Ok(aluno);
+            }
+            catch (System.Exception)
+            {
+
+                return BadRequest("Request Inválido");
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateAluno(Aluno aluno)
+        {
+            try
+            {
+                await _alunoService.CreateAluno(aluno);
+                return CreatedAtRoute(nameof(GetAluno), new { id = aluno.Id }, aluno);
+            }
+            catch (System.Exception)
+            {
+                return BadRequest("Request Inválido");
+            }
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult> DeleteAluno(int id)
+        {
+            try
+            {
+                var aluno = await _alunoService.GetAluno(id);
+                if (aluno != null)
+                {
+                    await _alunoService.DeleteAluno(aluno);
+                    return Ok($"Aluno foi deletado");
+                }
+                else
+                {
+                    return BadRequest("Request Inválido");
+                }
+            }
+            catch (System.Exception)
+            {
+                return BadRequest("Request Inválido");
+            }
         }
     }
 }
